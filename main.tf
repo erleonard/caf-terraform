30fd11d302b3bf32802cc20a00cf82a65157ee5c
Eric Leonard <ericleonard@microsoft.com>
2022-10-20 14:13:21 -0400

    fixed code... maybe

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 # Get the current client configuration from the AzureRM provider.
+# Get the current client configuration from the AzureRM provider.
 # This is used to populate the root_parent_id variable with the
 # current Tenant ID used as the ID for the "Tenant Root Group"
 # Management Group.
 
 data "azurerm_client_config" "core" {}
 
-# Declare the Terraform Module for Cloud Adoption Framework
-# Enterprise-scale and provide a base configuration.
+# Declare the Azure landing zones Terraform module
+# and provide a base configuration.
 
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
@@ -14,16 +15,15 @@ module "enterprise_scale" {
 
   providers = {
     azurerm              = azurerm
-    azurerm.connectivity = azurerm.connectivity
-    azurerm.management   = azurerm.management
-   }
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
 
   root_parent_id = data.azurerm_client_config.core.tenant_id
   root_id        = var.root_id
   root_name      = var.root_name
 
   deploy_core_landing_zones = true
-  
   deploy_management_resources    = var.deploy_management_resources
   subscription_id_management     = data.azurerm_client_config.core.subscription_id
   configure_management_resources = local.configure_management_resources
